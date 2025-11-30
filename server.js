85828e03abcc1e87dcd040dd518e53817d6d406b
Ganesh <ganeshbaskaran@qdexconsulting.com>
2025-11-30 16:25:12 +0530

    CRUD Implementation

--- server.js
+++ server.js
@@ -25,10 +25,7 @@ app.get('/', (req, res) => {
     message: 'Welcome to User CRUD API',
     endpoints: {
       'GET /api/users': 'Get all users',
-      'GET /api/users/:id': 'Get user by ID',
       'POST /api/users': 'Create new user',
-      'PUT /api/users/:id': 'Update user',
-      'DELETE /api/users/:id': 'Delete user',
     },
   });
 });
